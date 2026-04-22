c270e5a0fe66092221fa2fad269a86f8a1601250
huynt <huynt@qsys.com>
2026-04-22 13:51:47 +1000

    deploy dev env

--- providers.tf
+++ providers.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
   #beckend for push file terraform.tfstate to azure cloud# create storage first
-  # backend "azurerm" {
-  #   resource_group_name  = "nth-terraform-state"
-  #   storage_account_name = "stterraforstatenth"
-  #   container_name       = "tfstate"
-  #   key                  = "nth.terraform.tfstate" # name file will save on Cloud
-  # }
+  backend "azurerm" {
+    resource_group_name  = "nth-terraform-state"
+    storage_account_name = "stterraforstatenth"
+    container_name       = "tfstate"
+    key                  = "nth.terraform.tfstate" # name file will save on Cloud
+  }
 }
 
 provider "azurerm" {
